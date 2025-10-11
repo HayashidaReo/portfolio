9fc33811cb62cfd237c2a6f3798884a9f37e081a
hayashidareo <reo0524h@gmail.com>
2025-10-11 22:48:49 +0900

    不要なreactインポートを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/pages/HomePage';
 import { ProjectDetailPage } from './components/pages/ProjectDetailPage';
