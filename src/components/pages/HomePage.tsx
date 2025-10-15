3a55fae6c7f7212968b65fcc43e6b91a4ce14f8d
hayashidareo <reo0524h@gmail.com>
2025-10-15 20:43:28 +0900

    相対パスを使用している箇所を@に変更

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { PageLayout } from '../templates/PageLayout';
-import { Header } from '../organisms/Header';
-import { AboutSection } from '../organisms/sections/AboutSection';
-import { SkillsSection } from '../organisms/sections/SkillsSection';
-import { WorksSection } from '../organisms/sections/WorksSection';
-import { CareerSection } from '../organisms/sections/CareerSection';
-import { SocialSection } from '../organisms/sections/SocialSection';
-import { ContactSection } from '../organisms/sections/ContactSection';
+import { PageLayout } from '@/components/templates/PageLayout';
+import { Header } from '@/components/organisms/Header';
+import { AboutSection } from '@/components/organisms/sections/AboutSection';
+import { SkillsSection } from '@/components/organisms/sections/SkillsSection';
+import { WorksSection } from '@/components/organisms/sections/WorksSection';
+import { CareerSection } from '@/components/organisms/sections/CareerSection';
+import { SocialSection } from '@/components/organisms/sections/SocialSection';
+import { ContactSection } from '@/components/organisms/sections/ContactSection';
 
 export const HomePage: React.FC = () => {
   return (
